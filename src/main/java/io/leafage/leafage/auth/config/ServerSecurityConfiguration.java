5f831b4a8c1ac704157717564cfa0acfdd778f90
little3201 <little3201@163.com>
2021-11-18 18:04:38 +0800

    配置并测试通过

--- src/main/java/io/leafage/leafage/auth/config/ServerSecurityConfiguration.java
+++ src/main/java/io/leafage/leafage/auth/config/ServerSecurityConfiguration.java
@@ -1,13 +1,12 @@
 package io.leafage.leafage.auth.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -20,21 +19,16 @@ public class ServerSecurityConfiguration {
                 .authorizeRequests(authorizeRequests ->
                         authorizeRequests.anyRequest().authenticated()
                 )
-                .formLogin();
+                .formLogin(Customizer.withDefaults());
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 
     // 此处创建用户，张三。
     @Bean
     UserDetailsService users() {
-        UserDetails user = User.builder()
+        UserDetails user = User.withDefaultPasswordEncoder()
                 .username("zhangsan")
-                .password(passwordEncoder().encode("zhangsan123"))
+                .password("zhangsan123")
                 .roles("USER")
                 .build();
         return new InMemoryUserDetailsManager(user);
